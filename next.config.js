8008b9ea0ed4a47c5d720907739b290c733f88be
lunarscents <lunarscents7@gmail.com>
2020-02-13 20:24:41 +0900

    [env] - modify development environment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
-const withTypescript = require("@zeit/next-typescript");
+const withMDX = require('@next/mdx')();
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-module.exports = withTypescript({
-  webpack(config, options) {
+const hash = process.env.GIT_HASH;
+
+module.exports = withMDX({
+  webpack(config, { buildId, dev, isServer, webpack }) {
+    if(dev) {
+      // Disable minimizer
+      config.optimization.minimize = false;
+    }
+
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+        net: 'empty',
+      };
+    }
+
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+
     return config;
   },
   generateBuildId: () => hash,
-  pageExtensions: ['tsx']
+  pageExtensions: ['tsx', 'js', 'jsx', 'md', 'mdx']
 });
