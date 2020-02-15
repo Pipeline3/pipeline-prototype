c48760dcd5eb5c0027e090b039f56772538d7731
whis <whisdevj@gmail.com>
2020-02-15 17:35:06 +0900

    [standard] clean pages>index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,4 @@
-import useFetch from 'hooks/useFetch';
-
 const Index = () => {
-  const options = {
-    baseUrl: 'https://www.nlotto.co.kr',
-    method: 'GET',
-    url: '/common.do',
-    params: {
-      method: 'getLottoNumber',
-      drwNo: 588,
-    },
-  };
-
-  const [data] = useFetch(options);
-
-  if (!data) return <></>;
-
   return <h1> Hello </h1>;
 };
 
